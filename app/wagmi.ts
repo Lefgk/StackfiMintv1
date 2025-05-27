3c12da4ee37700ef4f325e46ca318ec7f09a5b3d
EG <37511853+Lefgk@users.noreply.github.com>
2025-05-27 10:45:57 +0100

    sd

--- app/wagmi.ts
+++ app/wagmi.ts
@@ -1,9 +1,16 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import { base } from "wagmi/chains";
+import { metaMaskWallet, rabbyWallet } from "@rainbow-me/rainbowkit/wallets";
 
 export const config = getDefaultConfig({
   appName: "StackFi NFT Mint",
   projectId: "YOUR_WALLETCONNECT_PROJECT_ID", // Get this from https://cloud.walletconnect.com
   chains: [base],
   ssr: true, // If your dApp uses server side rendering (SSR)
+  wallets: [
+    {
+      groupName: "Recommended",
+      wallets: [metaMaskWallet, rabbyWallet],
+    },
+  ],
 });
